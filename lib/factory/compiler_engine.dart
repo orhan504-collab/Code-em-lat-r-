5bc02b05aee2051522b8abf76ebe732be783e546
orhan504-collab <orhan_504@hotmail.com>
2026-03-10 23:15:12 +0300

    Update compiler_engine.dart

--- lib/factory/compiler_engine.dart
+++ lib/factory/compiler_engine.dart
@@ -1,7 +1,7 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class CompilerEngine {
-  // Buraya API anahtarın gelecek
+  // AIzaSyBdr-AigIGg31bMBhSB8bMxe5ghnC8DA28
   final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: 'API_KEY');
 
   Future<String> generateCode(String userRequest) async {
